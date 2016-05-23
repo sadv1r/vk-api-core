a388ac4df9463e84515ae8eaf717daafa4337553
sadv1r <sadv12@gmail.com>
2016-05-23 11:20:10 +0300

    Профиль дописан

--- src/main/kotlin/ru/sadv1r/vk/parser/model/Profile.kt
+++ src/main/kotlin/ru/sadv1r/vk/parser/model/Profile.kt
@@ -113,8 +113,75 @@ import java.sql.Timestamp
  *           Количество различных объектов у пользователя.
  *           Может быть использовано только в методе [users.get] при запросе информации об одном пользователе,
  *           с передачей **access_token**
- *
- * TODO дописать начиная со второй страницы
+ * @property occupation информация о текущем роде занятия пользователя
+ * @property nickname никнейм (отчество) пользователя
+ * @property relatives список родственников текущего пользователя
+ * @property relation
+ *           Семейное положение пользователя:
+ *           1 — не женат/не замужем;
+ *           2 — есть друг/есть подруга;
+ *           3 — помолвлен/помолвлена;
+ *           4 — женат/замужем;
+ *           5 — всё сложно;
+ *           6 — в активном поиске;
+ *           7 — влюблён/влюблена;
+ *           0 — не указано
+ * @property relationPartner партнер, указанный в семейном положении пользователя
+ * @property personal информация о полях из раздела «Жизненная позиция»
+ * @property skype **Skype** пользователя
+ * @property facebook **Facebook** пользователя
+ * @property twitter **Twitter** пользователя
+ * @property livejournal **Livejournal** пользователя
+ * @property instagram **Instagram** пользователя
+ * @property exports
+ *           Внешние сервисы,
+ *           в которые настроен экспорт из ВК (*twitter*, *facebook*, *livejournal*, *instagram*)
+ * @property wallComments доступно ли комментирование стены (*1* — доступно, *0* — недоступно)
+ * @property activities деятельность
+ * @property interests интересы
+ * @property music любимая музыка
+ * @property movies любимые фильмы
+ * @property tv любимые телешоу
+ * @property books любимые книги
+ * @property games любимые игры
+ * @property about «О себе»
+ * @property quotes любимые цитаты
+ * @property canPost
+ *           Информация о том, разрешено ли оставлять записи на стене у пользователя.
+ *           Возвращаемые значения: *1* —разрешено, *0* — не разрешено
+ * @property canSeeAllPosts
+ *           Информация о том, разрешено ли видеть чужие записи на стене пользователя.
+ *           Возвращаемые значения: *1* —разрешено, *0* — не разрешено
+ * @property canSeeAudio
+ *           Информация о том, разрешено ли видеть чужие аудиозаписи на стене пользователя.
+ *           Возвращаемые значения: *1* —разрешено, *0* — не разрешено
+ * @property canWritePrivateMessage
+ *           Информация о том, разрешено ли написание личных сообщений данному пользователю.
+ *           Возвращаемые значения: *1* —разрешено, *0* — не разрешено
+ * @property canSendFriendRequest
+ *           Информация о том, будет ли отправлено уведомление пользователю о заявке в друзья.
+ *           Возвращаемые значения: *1* — уведомление будет отправлено, *0* — уведомление не будет оптравлено
+ * @property isFavorite возвращается *1*, если пользователь находится в закладках у текущего пользователя
+ * @property isHiddenFromFeed возвращается *1*, если пользователь скрыт в новостях у текущего пользователя
+ * @property timezone временная зона пользователя. Возвращается только при запросе информации о текущем пользователе
+ * @property screenName короткое имя (поддомен) страницы пользователя
+ * @property maidenName девичья фамилия
+ * @property cropPhoto возвращает данные о точках, по которым вырезаны профильная и миниатюрная фотографии пользователя
+ * @property isFriend *1* – пользователь друг, *2* – пользователь не в друзьях
+ * @property friendStatus
+ *           Статус дружбы с пользователем:
+ *           0 – пользователь не является другом;
+ *           1 – отправлена заявка/подписка пользователю;
+ *           2 – имеется входящая заявка/подписка от пользователя;
+ *           3 – пользователь является другом
+ * @property career информация о карьере пользователя
+ * @property military информация о военной службе пользователя
+ * @property blacklisted
+ *           Возвращается 1, если текущий пользователь находится в
+ *           черном списке у запрашиваемого пользователя
+ * @property blacklistedByMe
+ *           Возвращается 1, если запрашиваемый пользователь находится в
+ *           черном списке у текущего пользователя
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Profile(
@@ -173,9 +240,61 @@ data class Profile(
         val followersCount: Int? = null,
         @JsonProperty("common_count")
         val commonCount: Int? = null,
-        val counters: Counters? = null
-
-        //TODO дописать начиная со второй страницы
+        val counters: Counters? = null,
+        val occupation: Occupation? = null,
+        val nickname: String? = null,
+        val relatives: List<Relatives>? = null,
+        val relation: Int? = null,
+        @JsonProperty("relation_partner")
+        val relationPartner: RelationPartner? = null,
+        val personal: Personal? = null,
+        val skype: String? = null,
+        val facebook: String? = null,
+        val twitter: String? = null,
+        val livejournal: String? = null,
+        val instagram: String? = null,
+        val exports: Any? = null, //FIXME разобраться с форматом
+        @JsonProperty("wall_comments")
+        val wallComments: Int? = null,
+        val activities: String? = null,
+        val interests: String? = null,
+        val music: String? = null,
+        val movies: String? = null,
+        val tv: String? = null,
+        val books: String? = null,
+        val games: String? = null,
+        val about: String? = null,
+        val quotes: String? = null,
+        @JsonProperty("can_post")
+        val canPost: Boolean? = null,
+        @JsonProperty("can_see_all_posts")
+        val canSeeAllPosts: Boolean? = null,
+        @JsonProperty("canSeeAudio")
+        val canSeeAudio: Boolean? = null,
+        @JsonProperty("can_write_private_message")
+        val canWritePrivateMessage: Boolean? = null,
+        @JsonProperty("can_send_friend_request")
+        val canSendFriendRequest: Boolean? = null,
+        @JsonProperty("is_favorite")
+        val isFavorite: Boolean? = null,
+        @JsonProperty("is_hidden_from_feed")
+        val isHiddenFromFeed: Boolean? = null,
+        val timezone: Int? = null,
+        @JsonProperty("screen_name")
+        val screenName: String? = null,
+        @JsonProperty("maiden_name")
+        val maidenName: String? = null,
+        @JsonProperty("crop_photo")
+        val cropPhoto: CropPhoto? = null,
+        @JsonProperty("is_friend")
+        val isFriend: Boolean? = null,
+        @JsonProperty("friend_status")
+        val friendStatus: Int? = null,
+        val career: List<Career>? = null,
+        val military: Military? = null,
+        val blacklisted: Boolean? = null,
+        @JsonProperty("blacklisted_by_me")
+        val blacklistedByMe: Boolean? = null
 ) {
     /**
      * @property DELETED страница пользователя удалена
@@ -332,5 +451,187 @@ data class Profile(
             val userVideos: Int,
             val followers: Int,
             val pages: Int
+    )
+
+    /**
+     * @property type тип занятия
+     * @property id идентификатор школы, вуза, группы компании (в которой пользователь работает)
+     * @property name название школы, вуза или места работы
+     */
+    data class Occupation(
+            val type: Type,
+            val id: Int,
+            val name: String
+    ) {
+        enum class Type {
+            @JsonProperty("work")
+            WORK,
+            @JsonProperty("school")
+            SCHOOL,
+            @JsonProperty("university")
+            UNIVERSITY
+        }
+    }
+
+    /**
+     * @property id идентификатор пользователя Вконтакте, являющегося родственником
+     * @property name когда родственник не является пользователем ВКонтакте (вместо [id])
+     * @property type тип родственных связей
+     */
+    data class Relatives(
+            val id: Int,
+            val name: String,
+            val type: Type
+    ) {
+        /**
+         * @property SIBLING брат/сестра
+         * @property PARENT родитель
+         * @property CHILD ребенок
+         * @property GRANDPARENT дедушка/бабушка
+         * @property GRANDCHILD внук
+         */
+        enum class Type {
+            @JsonProperty("sibling")
+            SIBLING,
+            @JsonProperty("parent")
+            PARENT,
+            @JsonProperty("child")
+            CHILD,
+            @JsonProperty("grandparent")
+            GRANDPARENT,
+            @JsonProperty("grandchild")
+            GRANDCHILD
+        }
+    }
+
+    /**
+     * @property id идентификатор пользователя Вконтакте, являющегося партнером
+     * @property name имя партнера
+     */
+    data class RelationPartner(
+            val id: Int,
+            val name: String
+    )
+
+    /**
+     * @property political:
+     *           Политические предпочтения
+     *           1 — коммунистические;
+     *           2 — социалистические;
+     *           3 — умеренные;
+     *           4 — либеральные;
+     *           5 — консервативные;
+     *           6 — монархические;
+     *           7 — ультраконсервативные;
+     *           8 — индифферентные;
+     *           9 — либертарианские
+     * @property langs языки
+     * @property religion мировоззрение
+     * @property inspiredBy источники вдохновения
+     * @property peopleMain
+     *           Главное в людях:
+     *           1 — ум и креативность;
+     *           2 — доброта и честность;
+     *           3 — красота и здоровье;
+     *           4 — власть и богатство;
+     *           5 — смелость и упорство;
+     *           6 — юмор и жизнелюбие
+     * @property lifeMain
+     *           Главное в жизни:
+     *           1 — семья и дети;
+     *           2 — карьера и деньги;
+     *           3 — развлечения и отдых;
+     *           4 — наука и исследования;
+     *           5 — совершенствование мира;
+     *           6 — саморазвитие;
+     *           7 — красота и искусство;
+     *           8 — слава и влияние;
+     * @property smoking
+     *           Отношение к курению:
+     *           1 — резко негативное;
+     *           2 — негативное;
+     *           3 — нейтральное;
+     *           4 — компромиссное;
+     *           5 — положительное
+     * @property alcohol
+     *           Отношение к алкоголю:
+     *           1 — резко негативное;
+     *           2 — негативное;
+     *           3 — нейтральное;
+     *           4 — компромиссное;
+     *           5 — положительное
+     */
+    data class Personal(
+            val political: Int,
+            val langs: List<String>,
+            val religion: String,
+            @JsonProperty("inspired_by")
+            val inspiredBy: String,
+            @JsonProperty("people_main")
+            val peopleMain: Int,
+            @JsonProperty("life_main")
+            val lifeMain: Int,
+            val smoking: Int,
+            val alcohol: Int
+    )
+
+    /**
+     * @property photo объект [Photo] фотографии пользователя из которой вырезается профильная аватарка
+     * @property crop вырезанная фотография пользователя, координаты указаны в процентах
+     * @property rect миниатюрная квадратная фотография, вырезанная из фотографии [crop], координаты указаны в процентах
+     */
+    data class CropPhoto(
+            val photo: Photo,
+            val crop: Coordinates,
+            val rect: Coordinates
+    ) {
+        data class Coordinates(
+                val x: Float,
+                val y: Float,
+                val x2: Float,
+                val y2: Float
+        )
+    }
+
+    /**
+     * @property groupId идентификатор сообщества (если доступно, иначе [company])
+     * @property company название организации (если доступно, иначе [groupId])
+     * @property countryId идентификатор страны
+     * @property cityId идентификатор города (если доступно, иначе [cityName])
+     * @property cityName название города (если доступно, иначе [cityId])
+     * @property from год начала работы
+     * @property until год окончания работы
+     * @property position должность
+     */
+    data class Career(
+            @JsonProperty("group_id")
+            val groupId: Int,
+            val company: String,
+            @JsonProperty("country_id")
+            val countryId: Int,
+            @JsonProperty("city_id")
+            val cityId: Int,
+            @JsonProperty("city_name")
+            val cityName: String,
+            val from: Int,
+            val until: Int,
+            val position: String
+    )
+
+    /**
+     * @property unit номер части
+     * @property unitId идентификатор части в базе данных
+     * @property countryId идентификатор страны, в которой находится часть
+     * @property from год начала службы
+     * @property unit год окончания службы
+     */
+    data class Military(
+            val unit: Int,
+            @JsonProperty("unit_id")
+            val unitId: Int,
+            @JsonProperty("country_id")
+            val countryId: Int,
+            val from: Int,
+            val until: Int
     )
 }
